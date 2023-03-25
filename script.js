656260336bafd93fbbb341cf5d70b72bd6c892cb
Ash2001s <128940264+Ash2001s@users.noreply.github.com>
2023-03-25 23:16:24 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showNote() {
+    document.getElementById("note-popup").style.display = "block";
+  }
+  
+  function closeNote() {
+    document.getElementById("note-popup").style.display = "none";
+  }
+  
